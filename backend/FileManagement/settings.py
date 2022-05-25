b1ae2e3b835276c77a59c96afd7269041c293ec5
hector <hector.alonso@osoigo.com>
2022-05-25 19:54:30 -0400

    BACKEND: paginacion a 10

--- backend/FileManagement/settings.py
+++ backend/FileManagement/settings.py
@@ -53,7 +53,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
          'rest_framework.permissions.IsAuthenticated'],
     'DEFAULT_PAGINATION_CLASS': 'FileApp.pagination.StandardResultsSetPagination',
-    'PAGE_SIZE': 1,
+    'PAGE_SIZE': 10,
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.SessionAuthentication',
